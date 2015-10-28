7f51488dbd0b1f2dea6742776899e6436b511a82
greensea <gs@bbxy.net>
2015-10-28 15:06:24 +0800

    增加 v3 版本的 API 目录

---
+++ api/v3/last_location.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * 获取用户最新的位置信息
+ */
+require_once('../../header.php');
+
+if ($my->connect_error) {
+    apiout(-1, $my->connect_error);
+    die();
+}
+
+
+$uid = $_GET['uid'];
+
+$uid_qs = $my->real_escape_string($uid);
+
+$sql = "SELECT * FROM b_user WHERE uid='{$uid_qs}'";
+$res = $my->query($sql);
+if (!$res) {
+    apiout(-2, $my->error . "(${sql}");
+    die();
+}
+$user = $res->fetch_array();
+if (!$user) {
+    apiout(-3, "uid=`{$uid}'的用户不存在");
+    die();
+}
+
+$google_uid_qs = $my->real_escape_string($user['google_uid']);
+
+
+$sql = "SELECT * FROM b_location WHERE google_uid='{$google_uid_qs}' ORDER BY rtime DESC LIMIT 1";
+$res = $my->query($sql);
+if (!$res) {
+    apiout(-4, $my->error . "(${sql})");
+    die();
+}
+
+$loc = $res->fetch_array();
+if (!$loc) {
+    apiout(-4, "uid=`{$uid}' 的用户未上报任何位置(google_uid=`{$user['google_uid']}')");
+    die();
+}
+else {
+    apiout(0, "操作成功", $loc);
+    die();
+}
+?>
+
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 require_once('lib/func.php');
 
-$my = new mysqli('localhost', 'latitude', 'WZCy8uR63pvdAdRN', 'latitude');
+$my = new mysqli('latitude.greensea.org', 'latitude', 'WZCy8uR63pvdAdRN', 'latitude');
 
 $GOOGLE_CLIENT_ID = '1026836454706-vmm6iis0p8rsic5p7786ov7nrh04f1ee.apps.googleusercontent.com';
 
