2e7494d5952bdf3cd3be9b327e856d15e70e1857
greensea <gs@bbxy.net>
2015-10-31 13:52:56 +0800

    locations 接口增加我的轨迹信息的输出

--- api/v3/friend/locations.php
+++ api/v3/friend/locations.php
@@ -12,8 +12,36 @@ if (!$user) {
     apiout(-2, '你还没有登录');
 }
 
+/// 获取好友位置信息
 $friends = getFriendsWithLocationByGoogleUID($user['google_uid']);
 
+
+/// 获取我的轨迹
+$where = [
+    'AND' => [
+        'google_uid' => $user['google_uid'],
+        'rtime[>]' => time() - 86400,
+    ],
+    'ORDER' => 'rtime ASC'
+];
+$tracks = $db->select('b_location', ['latitude', 'longitude', 'accuracy', 'rtime'], $where);
+if (!$tracks) {
+    $tracks = array();
+}
+
+/// 精简轨迹，最多返回 30 个轨迹信息
+if (count($tracks) > 30) {
+    $step = round(count($tracks) / 30) + 1;
+    for ($i = 0; $i < count($tracks) - 1; $i += $step) {
+        unset($tracks[$i]);
+    }
+}
+    
+
+$data = [
+    'tracks' => $tracks,
+    'friends' => $friends
+];
 
 apiout(0, '操作成功', $friends);
 ?>
