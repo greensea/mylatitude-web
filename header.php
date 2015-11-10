cf917c2e33502512acdd82dbb73aec6b7169b3a9
greensea <gs@bbxy.net>
2015-11-10 17:30:49 +0800

    将配置文件从 GIT 中分离出来

--- header.php
+++ header.php
@@ -2,18 +2,24 @@
 require_once('lib/func.php');
 require_once('lib/medoo.php');
 
-$my = new mysqli('latitude.greensea.org', 'latitude', 'WZCy8uR63pvdAdRN', 'latitude');
+if (!file_exists(__DIR__ . '/config.php')) {    
+    die('请先创建配置文件 config.php，配置文件模版可以使用　config.sample.php');
+}
+else {
+    require_once('config.php');
+}
+
+
+$my = new mysqli($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE);
 $db = new medoo([
     'database_type' => 'mysql',
-    'database_name' => 'latitude',
-    'server' => 'latitude.greensea.org',
-    'username' => 'latitude',
-    'password' => 'WZCy8uR63pvdAdRN',
+    'database_name' => $MYSQL_DATABASE,
+    'server' => $MYSQL_HOST,
+    'username' => $MYSQL_USER,
+    'password' => $MYSQL_PASSWORD,
     'charset' => 'utf8'
 ]);
     
-
-$GOOGLE_CLIENT_ID = '1026836454706-vmm6iis0p8rsic5p7786ov7nrh04f1ee.apps.googleusercontent.com';
 
 $GOOGLE_JWT_KEYS = google_jwt_keys();
 
